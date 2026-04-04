99288c3b0574a042fe1f3528d2fd99be5d50d611
ercanaziz <ercanaziz08@gmail.com>
2026-04-05 00:13:11 +0300

    backend kodları eklendi

---
+++ Ercan-Aziz/Backend/db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var ProductCollection  *mongo.Collection
+var FeedbackCollection *mongo.Collection
+var PlanCollection     *mongo.Collection
+
+func Connect() {
+	uri := os.Getenv("MONGO_URI")
+    if uri == "" {
+    log.Fatal("🚨 HATA: MONGO_URI bulunamadı!")
+    }
+    clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = client.Ping(context.TODO(), nil); err != nil {
+		log.Fatal("MongoDB bağlantısı başarısız!", err)
+	}
+	fmt.Println("✅ MongoDB Bağlantısı Başarılı!")
+
+	priceDB  := client.Database("priceTracker_DB")
+	FeedbackCollection = priceDB.Collection("feedback")
+	PlanCollection     = priceDB.Collection("subscription_plans")
+
+	notifyDB := client.Database("notiFY_DB")
+	ProductCollection  = notifyDB.Collection("watchlist")
+}
--- Ercan-Aziz/Backend/models/models.go
+++ Ercan-Aziz/Backend/models/models.go
@@ -1,21 +1,10 @@
-package main
+package models
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
 	"time"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-
 
 type Product struct {
 	ID           primitive.ObjectID `json:"id"            bson:"_id,omitempty"`
@@ -35,13 +24,13 @@ type Feedback struct {
 	UserID      string             `json:"user_id"      bson:"user_id"`
 	Subject     string             `json:"subject"      bson:"subject"`
 	Message     string             `json:"message"      bson:"message"`
-	Type        string             `json:"type"         bson:"type"` // "bug" | "suggestion" | "other"
+	Type        string             `json:"type"         bson:"type"`
 	SubmittedAt time.Time          `json:"submitted_at" bson:"submitted_at"`
 }
 
 type SubscriptionPlan struct {
 	ID          primitive.ObjectID `json:"id"           bson:"_id,omitempty"`
-	Name        string             `json:"name"         bson:"name"` // "free" | "basic" | "pro" | "enterprise"
+	Name        string             `json:"name"         bson:"name"`
 	MaxTracking int                `json:"max_tracking" bson:"max_tracking"`
 	Price       float64            `json:"price"        bson:"price"`
 	UpdatedAt   time.Time          `json:"updated_at"   bson:"updated_at"`
