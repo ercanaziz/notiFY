f443d719b8f507d2b0299855f2412e0c67f45fbf
ercanaziz <ercanaziz08@gmail.com>
2026-04-05 12:36:42 +0300

    update

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-    feedback "github.com/ercanaziz/notiFY/Ercan-Aziz/Backend"
+	feedback "github.com/ercanaziz/notiFY/Ercan-Aziz/Backend"
+	history "github.com/ercanaziz/notiFY/Sema-Durgut"
 )
 
 func main() {
-       
-    feedback.Start()
-    select {}   
-}      
-       
+
+	feedback.Start()
+	history.Start()
+	select {}
+}
