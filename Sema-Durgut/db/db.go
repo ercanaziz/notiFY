44953ef5fe60b841c88d5386f07a6168a19e1f65
semadurgut <164044552+semadurgut@users.noreply.github.com>
2026-04-05 11:21:44 +0300

    db güncellendi

--- Sema-Durgut/db/db.go
+++ Sema-Durgut/db/db.go
@@ -7,41 +7,43 @@ import (
 	"os"
 	"time"
 
+	"github.com/joho/godotenv" // BU SATIR EKSİKTİ: .env okumak için gerekli
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Global değişkenlerimiz (mongo.Database kullanıyoruz)
 var MongoClient *mongo.Client
 var DB *mongo.Database
 
 func Connect() {
-	//  MONGO_URI kullanacağız
+	// .env dosyasını yükle (Bu kısım eklenmeli)
+	// Dosya yolunu bir üst klasörde olduğu için "../.env" veya direkt ".env" denemelisin
+	err := godotenv.Load(".env")
+	if err != nil {
+		// Eğer .env bulunamazsa en azından log basalım ama durmayalım (belki sistemde tanımlıdır)
+		fmt.Println(".env dosyası yüklenemedi, sistem değişkenlerine bakılıyor...")
+	}
+
 	uri := os.Getenv("MONGO_URI")
 	if uri == "" {
 		log.Fatal("MONGO_URI ortam değişkeni bulunamadı!")
 	}
 
-	// Bağlantı için 10 saniyelik bir zaman aşımı süresi belirliyoruz
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	// Atlas'a bağlan
 	clientOptions := options.Client().ApplyURI(uri)
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal("MongoDB bağlantı hatası: ", err)
 	}
 
-	// Bağlantının gerçekten kurulup kurulmadığını test et (Ping at)
 	err = client.Ping(ctx, nil)
 	if err != nil {
 		log.Fatal("MongoDB Atlas'a ulaşılamıyor: ", err)
 	}
 
 	MongoClient = client
-
-	// Ekran görüntüsündeki veritabanı adını tam buraya yazıyoruz
 	DB = client.Database("notiFY_DB")
 
 	fmt.Println("MongoDB Atlas bağlantısı başarıyla kuruldu!")
